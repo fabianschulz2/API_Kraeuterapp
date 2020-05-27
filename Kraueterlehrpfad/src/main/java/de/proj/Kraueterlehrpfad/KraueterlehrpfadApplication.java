a4c6af6bd7929c965fff0ab59480c46afc89368c
Fabian Schulz <fabian.schulz2@hotmail.de>
2020-05-27 09:53:58 +0200

    Project initialize

---
+++ Kraueterlehrpfad/src/main/java/de/proj/Kraueterlehrpfad/KraueterlehrpfadApplication.java
@@ -0,0 +1,13 @@
+package de.proj.Kraueterlehrpfad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KraueterlehrpfadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KraueterlehrpfadApplication.class, args);
+	}
+
+}
---
+++ Kraueterlehrpfad/src/test/java/de/proj/Kraueterlehrpfad/KraueterlehrpfadApplicationTests.java
@@ -0,0 +1,13 @@
+package de.proj.Kraueterlehrpfad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KraueterlehrpfadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
