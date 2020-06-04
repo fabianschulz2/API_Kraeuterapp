af14029ca113d885520104924ec19759727d6cc0
ValentinJung <valentin.jung@th-bingen.de>
2020-06-04 16:25:41 +0200

    Auf echte Datenbank umgeschrieben

--- Kraueterlehrpfad/src/main/java/de/proj/Kraueterlehrpfad/Entity/Kraeuter.java
+++ Kraueterlehrpfad/src/main/java/de/proj/Kraueterlehrpfad/Entity/Kraeuter.java
@@ -1,69 +1,203 @@
 package de.proj.Kraueterlehrpfad.Entity;
 
 import com.sun.istack.NotNull;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.web.ConditionalOnEnabledResourceChain;
-
 
 import javax.persistence.*;
-import java.util.HashMap;
-import java.util.Map;
 
 @Entity
-@Table(name="Kraeuter")
+@Table(name="kraeuter")
 
 public class Kraeuter {
 
+    //Gleiche Bezeichner wie in DB
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Kraeuter_ID;
+    @NotNull
+    private String Name;
+    @NotNull
+    private String Lateinischer_Name;
+    @NotNull
+    private String Familie;
+    @NotNull
+    private String Wuchs;
+    @NotNull
+    private Integer Bluetenzeit;
+    @NotNull
+    private String Blueten;
+    @NotNull
+    private String Ausbreitung;
+    @NotNull
+    private String Vorkommen;
+    private Integer Samenreife;
+    private Integer Fruchtreife;
+    private String Fruechte;
+    private String Bestaeuber;
+    private String Insektenarten;
+    private String Schmetterlingsfutterpflanze;
+    private String Heilanwendung;
+    private String Inhaltsstoffe;
 
-    private Integer id;
-    @NotNull
-    private String name;
-    @NotNull
-    private String latName;
-    @NotNull
-    private String eigenschaften;
+
 
     public Kraeuter(){
 
     }
 
-    public Kraeuter(String name, String latName, String eigenschaften) {
-        this.name = name;
-        this.latName = latName;
-        this.eigenschaften = eigenschaften;
+    public Kraeuter(Integer kraeuter_ID, String name, String lateinischer_Name, String familie, String wuchs, Integer bluetenzeit, String blueten, String ausbreitung, String vorkommen, Integer samenreife, Integer fruchtreife, String fruechte, String bestaeuber, String insektenarten, String schmetterlingsfutterpflanze, String heilanwendung, String inhaltsstoffe) {
+        Kraeuter_ID = kraeuter_ID;
+        Name = name;
+        Lateinischer_Name = lateinischer_Name;
+        Familie = familie;
+        Wuchs = wuchs;
+        Bluetenzeit = bluetenzeit;
+        Blueten = blueten;
+        Ausbreitung = ausbreitung;
+        Vorkommen = vorkommen;
+        Samenreife = samenreife;
+        Fruchtreife = fruchtreife;
+        Fruechte = fruechte;
+        Bestaeuber = bestaeuber;
+        Insektenarten = insektenarten;
+        Schmetterlingsfutterpflanze = schmetterlingsfutterpflanze;
+        Heilanwendung = heilanwendung;
+        Inhaltsstoffe = inhaltsstoffe;
+    }
+
+    public Integer getKraeuter_ID() {
+        return Kraeuter_ID;
+    }
+
+    public void setKraeuter_ID(Integer kraeuter_ID) {
+        Kraeuter_ID = kraeuter_ID;
     }
 
     public String getName() {
-        return name;
+        return Name;
     }
 
     public void setName(String name) {
-        this.name = name;
+        Name = name;
     }
 
-    public String getLatName() {
-        return latName;
+    public String getLateinischer_Name() {
+        return Lateinischer_Name;
     }
 
-    public void setLatName(String latName) {
-        this.latName = latName;
+    public void setLateinischer_Name(String lateinischer_Name) {
+        Lateinischer_Name = lateinischer_Name;
     }
 
-    public Integer getId() {
-        return id;
+    public String getFamilie() {
+        return Familie;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setFamilie(String familie) {
+        Familie = familie;
     }
 
-    public String getEigenschaften() {
-        return eigenschaften;
+    public String getWuchs() {
+        return Wuchs;
     }
 
-    public void setEigenschaften(String eigenschaften) {
-        this.eigenschaften = eigenschaften;
+    public void setWuchs(String wuchs) {
+        Wuchs = wuchs;
+    }
+
+    public Integer getBluetenzeit() {
+        return Bluetenzeit;
+    }
+
+    public void setBluetenzeit(Integer bluetenzeit) {
+        Bluetenzeit = bluetenzeit;
+    }
+
+    public String getBlueten() {
+        return Blueten;
+    }
+
+    public void setBlueten(String blueten) {
+        Blueten = blueten;
+    }
+
+    public String getAusbreitung() {
+        return Ausbreitung;
+    }
+
+    public void setAusbreitung(String ausbreitung) {
+        Ausbreitung = ausbreitung;
+    }
+
+    public String getVorkommen() {
+        return Vorkommen;
+    }
+
+    public void setVorkommen(String vorkommen) {
+        Vorkommen = vorkommen;
+    }
+
+    public Integer getSamenreife() {
+        return Samenreife;
+    }
+
+    public void setSamenreife(Integer samenreife) {
+        Samenreife = samenreife;
+    }
+
+    public Integer getFruchtreife() {
+        return Fruchtreife;
+    }
+
+    public void setFruchtreife(Integer fruchtreife) {
+        Fruchtreife = fruchtreife;
+    }
+
+    public String getFruechte() {
+        return Fruechte;
+    }
+
+    public void setFruechte(String fruechte) {
+        Fruechte = fruechte;
+    }
+
+    public String getBestaeuber() {
+        return Bestaeuber;
+    }
+
+    public void setBestaeuber(String bestaeuber) {
+        Bestaeuber = bestaeuber;
+    }
+
+    public String getInsektenarten() {
+        return Insektenarten;
+    }
+
+    public void setInsektenarten(String insektenarten) {
+        Insektenarten = insektenarten;
+    }
+
+    public String getSchmetterlingsfutterpflanze() {
+        return Schmetterlingsfutterpflanze;
+    }
+
+    public void setSchmetterlingsfutterpflanze(String schmetterlingsfutterpflanze) {
+        Schmetterlingsfutterpflanze = schmetterlingsfutterpflanze;
+    }
+
+    public String getHeilanwendung() {
+        return Heilanwendung;
+    }
+
+    public void setHeilanwendung(String heilanwendung) {
+        Heilanwendung = heilanwendung;
+    }
+
+    public String getInhaltsstoffe() {
+        return Inhaltsstoffe;
+    }
+
+    public void setInhaltsstoffe(String inhaltsstoffe) {
+        Inhaltsstoffe = inhaltsstoffe;
     }
 }
--- Kraueterlehrpfad/src/main/java/de/proj/Kraueterlehrpfad/controller/KaeuterController.java
+++ Kraueterlehrpfad/src/main/java/de/proj/Kraueterlehrpfad/controller/KaeuterController.java
@@ -4,9 +4,7 @@ import de.proj.Kraueterlehrpfad.Entity.Kraeuter;
 import de.proj.Kraueterlehrpfad.repository.KraeuterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,8 +22,6 @@ public class KaeuterController {
         path="/kraut",
         produces= MediaType.APPLICATION_JSON_VALUE)
 
-
-
     public List<Kraeuter> getKraeuter() {
 
 
@@ -34,4 +30,49 @@ public class KaeuterController {
         return kraeuterList;
     }
 
+    @RequestMapping(
+            method = RequestMethod.GET,
+            path="/kraut",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Kraeuter updateKraeuter(@RequestBody Kraeuter kraeuter){
+        Kraeuter updateKraeuter = kraeuterRepository.getOne(kraeuter.getKraeuter_ID());
+        updateKraeuter.setAusbreitung(kraeuter.getAusbreitung());
+        updateKraeuter.setBestaeuber(kraeuter.getBestaeuber());
+        updateKraeuter.setBlueten(kraeuter.getBlueten());
+        updateKraeuter.setBluetenzeit(kraeuter.getBluetenzeit());
+        updateKraeuter.setFamilie(kraeuter.getFamilie());
+        updateKraeuter.setFruchtreife(kraeuter.getFruchtreife());
+        updateKraeuter.setFruechte(kraeuter.getFruechte());
+        updateKraeuter.setHeilanwendung(kraeuter.getHeilanwendung());
+        updateKraeuter.setInhaltsstoffe(kraeuter.getInhaltsstoffe());
+        updateKraeuter.setInsektenarten(kraeuter.getInsektenarten());
+        updateKraeuter.setLateinischer_Name(kraeuter.getLateinischer_Name());
+        updateKraeuter.setName(kraeuter.getName());
+        updateKraeuter.setSamenreife(kraeuter.getSamenreife());
+        updateKraeuter.setSchmetterlingsfutterpflanze(kraeuter.getSchmetterlingsfutterpflanze());
+        updateKraeuter.setVorkommen(kraeuter.getVorkommen());
+        updateKraeuter.setWuchs(kraeuter.getWuchs());
+        return kraeuterRepository.saveAndFlush(updateKraeuter);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            path="/kraut",
+            produces=MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Kraeuter saveKraeuter(@RequestBody Kraeuter kraeuter){
+        return kraeuterRepository.save(kraeuter);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.DELETE,
+            path="/kraut/{id}"
+    )
+    public void deleteKraeuter(@PathVariable("id") Integer id){
+        kraeuterRepository.deleteById(id);
+    }
 }
+
