cf3e271affc43aa957ab675064ea00f7dc0f2947
Fabian Schulz <46343861+fabianschulz2@users.noreply.github.com>
2020-06-19 15:35:27 +0200

    Funktioniert

--- Kraueterlehrpfad/src/main/java/de/proj/Kraueterlehrpfad/controller/PointController.java
+++ Kraueterlehrpfad/src/main/java/de/proj/Kraueterlehrpfad/controller/PointController.java
@@ -1,10 +1,22 @@
 package de.proj.Kraueterlehrpfad.controller;
 
+import de.proj.Kraueterlehrpfad.repository.PointRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PointController {
     @Autowired
-    PointController pointController;
+    PointRepository pointRepository;
+
+    @RequestMapping(
+            method = RequestMethod.DELETE,
+            path = "/point/{id}"
+    )
+    public void deleteKraut(@PathVariable("id") Integer id) {
+        pointRepository.deleteById(id);
+    }
 }
